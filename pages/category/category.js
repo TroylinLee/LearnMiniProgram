202fb284484df925909a63baeba60fd7efd4b23b
TroylinLee <361719496@qq.com>
2020-11-25 19:57:38 +0800

    初识小程序

--- pages/category/category.js
+++ pages/category/category.js
@@ -1,35 +1,11 @@
-// pages/home/home.js
+// pages/category/category.js
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-    name: 'CoderWhy',
-    age: 18,
-    students: [
-      {id: 110, name: 'kobe', age: 30},
-      {id: 111, name: 'james', age: 28},
-      {id: 112, name: 'curry', age: 32},
-      {id: 113, name: 'why', age: 18},
-    ],
-    counter: 0
-  },
-  PLUS() {
-    // 1. 错误做法
-    // this.data.counter += 1
-    // console.log(this.data.counter)
 
-    // 2. this.setData()
-    this.setData({
-      counter: this.data.counter + 1
-    })
-    
-  },
-  MINUS() {
-    this.setData({
-      counter: this.data.counter - 1
-    })
   },
 
   /**
